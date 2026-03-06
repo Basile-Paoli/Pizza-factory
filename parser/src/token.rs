5569c95ea519ac38c8161f0c7b9f827831fe39a7
Spatulox <M78stormtrooper@laposte.net>
2026-03-06 16:41:21 +0100

    tokeniser print the string

--- parser/src/token.rs
+++ parser/src/token.rs
@@ -13,7 +13,27 @@ pub enum Token {
     Newline,
 }
 
-struct Tokenizer<'a> {
+pub struct Tokenizer<'a> {
     input: &'a str,
     pos: usize,
+}
+
+impl<'a> Tokenizer<'a> {
+    pub fn new(input: &'a str) -> Self {
+        Self { input, pos: 0 }
+    }
+
+    pub fn parse(&mut self) -> Option<Token> {
+        loop {
+            self.skip_whitespace();
+            print!("{}", self.input.as_bytes()[self.pos] as char);
+            self.pos += 1
+        }
+    }
+
+    fn skip_whitespace(&mut self) {
+        while self.pos < self.input.len() && self.input.as_bytes()[self.pos] as char == ' ' {
+            self.pos += 1;
+        }
+    }
 }
