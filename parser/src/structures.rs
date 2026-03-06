d971ac6acd030b52e615510b8cc804a3aa807a6e
Spatulox <M78stormtrooper@laposte.net>
2026-03-06 16:17:33 +0100

    add the return struct

---
+++ parser/src/structures.rs
@@ -0,0 +1,24 @@
+#[derive(Debug, Clone)]
+pub enum BaseType {
+    Tomato,
+    Cream,
+}
+
+#[derive(Debug, Clone)]
+pub enum Step {
+    MakeDough,
+    AddBase { base_type: BaseType },
+    AddMushrooms { amount: u32 },
+    AddCheese { amount: u32 },
+    AddPepperoni { slices: u32 },
+    AddGarlic { cloves: u32 },
+    AddOregano { amount: u32 },
+    AddBasil { leaves: u32 },
+    AddOliveOil,
+}
+
+#[derive(Debug, Clone)]
+pub struct Recipe {
+    pub name: String,
+    pub steps: Vec<Step>,
+}
