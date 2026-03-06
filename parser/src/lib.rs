2f87b2b1c0d522bc374abed9642a39c0d91b3ae1
Spatulox <M78stormtrooper@laposte.net>
2026-03-06 16:41:33 +0100

    add the test for print to string

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -1,17 +1,16 @@
 mod structures;
 mod token;
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+mod test;
 
 #[cfg(test)]
 mod tests {
+    use crate::token::Tokenizer;
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_tokens() {
+        let input = " coucou cou cou";
+        let mut t = Tokenizer::new(input);
+        t.parse();
     }
 }
