de09efbb493ba63500106b2a688e215149c79836
Spatulox <M78stormtrooper@laposte.net>
2026-03-10 19:57:17 +0100

    Update structures.rs
    Adds `Bake` step variant.
    Implements `Recipe` printing methods for improved readability.

--- parser/src/structures.rs
+++ parser/src/structures.rs
@@ -17,10 +17,36 @@ pub enum Step {
     AddOregano { amount: u32 },
     AddBasil { leaves: u32 },
     AddOliveOil,
+    Bake { duration: u32 }
 }
 
 #[derive(Debug, Clone, Default)]
 pub struct Recipe {
     pub name: String,
     pub steps: Vec<Step>,
+}
+
+impl Recipe {
+
+    fn print_step(&self,step: &Step) {
+        match step {
+            Step::MakeDough => println!("🥖 MakeDough"),
+            Step::AddBase { base_type } => println!("🍕 AddBase({base_type:?})"),
+            Step::AddMushrooms { amount } => println!("🍄 AddMushrooms(amount: {amount})"),
+            Step::AddCheese { amount } => println!("🧀 AddCheese(amount: {amount})"),
+            Step::AddPepperoni { slices } => println!("🌭 AddPepperoni(slices: {slices})"),
+            Step::AddGarlic { cloves } => println!("🧄 AddGarlic(cloves: {cloves})"),
+            Step::AddOregano { amount } => println!("🌿 AddOregano(amount: {amount})"),
+            Step::AddBasil { leaves } => println!("🌱 AddBasil(leaves: {leaves})"),
+            Step::AddOliveOil => println!("🛢️ AddOliveOil"),
+            _ => {}
+        }
+    }
+    pub fn print_recipe(&self) {
+        println!("Recette: {:?}", self.name);
+        for (i, step) in self.steps.iter().enumerate() {
+            print!("  {}. ", i + 1);
+            self.print_step(step);
+        }
+    }
 }
