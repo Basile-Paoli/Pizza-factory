002c2cd80676d2187095847265ab23c89e879c8b
Spatulox <M78stormtrooper@laposte.net>
2026-03-12 19:28:57 +0100

    wip test

--- parser/src/test.rs
+++ parser/src/test.rs
@@ -1,24 +1,92 @@
-mod token;
-mod structures;
-mod parser;
+#[cfg(test)]
+mod tests {
+    use crate::{PizzaParser, Recipe};
 
-fn main() {
-    use crate::token::Tokenizer;
-    use crate::parser::Parser;
+    #[test]
+    fn test_funghi() {
+        let input = "Funghi =
+    MakeDough
+    -> AddBase(base_type=tomato)
+    -> AddMushrooms(amount=3)
+    -> AddCheese(amount=2)
+    -> Bake(duration=6)
+    -> AddOliveOil";
 
-    let input = "Margherita =
+        let recipes = PizzaParser::parse(input).unwrap();
+        let recipe = &recipes[0];
+        recipe.print_recipe();
+        assert_eq!(recipe.name, "Funghi");
+        assert_eq!(recipe.steps.len(), 6);  // 6 steps attendus
+    }
+
+    #[test]
+    fn test_pepperoni() {
+        let input = "Pepperoni =
+    MakeDough
+    -> AddBase(base_type=tomato)
+    -> AddCheese(amount=2)
+    -> AddPepperoni(slices=12)
+    -> Bake(duration=6)";
+
+        let recipes = PizzaParser::parse(input).unwrap();
+        assert_eq!(recipes[0].name, "Pepperoni");
+    }
+
+    #[test]
+    fn test_marinara() {
+        let input = "Marinara =
+    MakeDough
+    -> AddBase(base_type=tomato)
+    -> AddGarlic(cloves=2)
+    -> AddOregano(amount=1)
+    -> Bake(duration=5)
+    -> AddOliveOil";
+
+        let recipes = PizzaParser::parse(input).unwrap();
+        assert_eq!(recipes[0].name, "Marinara");
+    }
+
+    #[test]
+    fn test_margarita() {
+        let input = "Margherita =
     MakeDough
     -> AddBase(base_type=tomato)
     -> [AddCheese(amount=2), AddBasil(leaves=3)]
     -> Bake(duration=5)
-    -> AddPepperoni(slices=1)^12
     -> AddOliveOil";
-    let mut t = Tokenizer::new(input);
-    //t.print_tokens();
 
-    let mut p = Parser::new(t.parse());
-    match p.parse_recipe() {
-        Ok(recipes) => println!("Recipes: {:#?}", recipes),
-        Err(e) => println!("ERROR: {}", e),
+        let recipes = PizzaParser::parse(input).unwrap();
+        let recipe = &recipes[0];
+        recipe.print_recipe();
+        assert_eq!(recipe.steps.len(), 5);  // 1 MakeDough + 1 Multiple + 1 Bake + 1 OliveOil
+    }
+
+    #[test]
+    fn test_quattro_formaggi() {
+        let input = "QuattroFormaggi =
+    MakeDough
+    -> AddBase(base_type=cream)
+    -> AddCheese(amount=1)^4
+    -> Bake(duration=6)
+    -> AddOliveOil";
+
+        let recipes = PizzaParser::parse(input).unwrap();
+        let recipe = &recipes[0];
+        recipe.print_recipe();
+    }
+
+    #[test]
+    fn test_custom() {
+        let input = "Custom =
+    MakeDough
+    -> AddBase(base_type=cream)
+    -> [AddCheese(amount=1)^4, AddBasil(leaves=1)^4]
+    -> Bake(duration=6)
+    -> AddOliveOil";
+
+        let recipes = PizzaParser::parse(input).unwrap();
+        assert_eq!(recipes[0].name, "Custom");
     }
 }
+
+fn main() {}
